44a17e31e0a08476f2ee39248cb734fdc91c6211
adebisioje <bisioje@gmail.com>
2016-09-22 17:54:35 -0500

    Powershell Script: Classic to ARM
    Powershell Script to resources from Classic to ARM

---
+++ Classic_to_ARM_Script.ps1
@@ -0,0 +1,27 @@
+﻿#Sign in to your account for the Resource Manager model.
+Login-AzureRmAccount
+
+#Set your Azure subscription for the current session
+$subID=Get-AzureRmSubscription|Select-AzureRmSubscription
+
+
+#Register with the migration resource provider.
+Register-AzureRmResourceProvider -ProviderNamespace Microsoft.ClassicInfrastructureMigrate
+
+#check the registration
+Get-AzureRmResourceProvider -ProviderNamespace Microsoft.ClassicInfrastructureMigrate
+
+#sign in the classic model
+Add-AzureAccount
+
+#Set your Azure subscription for the current session
+$subID=Get-AzureRmSubscription|Select-AzureRmSubscription
+
+#Prepare the virtual network for migration
+$vnetName = "VNET001"
+Move-AzureVirtualNetwork -Prepare -VirtualNetworkName $vnetName
+
+#Commit the migration
+Move-AzureVirtualNetwork -Commit -VirtualNetworkName $vnetName
+
+
